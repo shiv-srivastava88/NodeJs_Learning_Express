09716f441f1b6cbb8a89448909297e307ab73e2e
Shivansh Srivastava <shiv.srivastava88@gmail.com>
2020-06-14 12:26:52 +0530

    Assigment 1

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promotionRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -13,7 +15,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes' , dishRouter);
-
+app.use('/promotions' , promotionRouter);
+app.use('/leaders' , leaderRouter);
 
 // app.get('/dishes/:dishId', (req,res,next) => {
 //     res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
